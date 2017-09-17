4ad4221987dbea471029ce860bf31859e1c15f5f
Mumffy <mumffy@gmail.com>
2017-09-16 21:14:31 -0700

    redoing some basics

---
+++ CtciCsharp/Q01_IsUnique.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CtciCsharp
+{
+    public class Ch01
+    {
+        public bool Q01_IsUnique(string input)
+        {
+            List<char> seenChars = new List<char>();
+            foreach (char c in input)
+            {
+                // TODO List.Contains vs Dict.Contains runtime?
+                if (seenChars.Contains(c))
+                {
+                    return false;
+                }
+                seenChars.Add(c);
+           } 
+            return true;
+        }
+
+        public bool Q01X_IsUnique_NoAddDSes(string input)
+        {
+            // BF: foreach char in string, walk rest of string to check for same char
+            // better: do in-place sort on the string - can achieve O(nlogn) with Heapsort,
+            //   then just walk through now-sorted string, and check adjacent chars for dupes
+            throw new NotImplementedException();
+        }
+
+        public bool Q02_CheckPermutation(string x, string y)
+        {
+            // ? case sensitivity?
+
+            // BF: could just walk through each string and count char occurrances for 2*O(n) + C
+            // less space: sort each string in-place for 2*O(nlogn), then see if two strings are same
+
+            if (x.Length != y.Length)
+            {
+                return false;
+            }
+
+            Dictionary<char, int> xCharCount = new Dictionary<char, int>();
+            Dictionary<char, int> yCharCount = new Dictionary<char, int>();
+            Q02_CountChars(x, xCharCount);
+            Q02_CountChars(y, yCharCount);
+
+            foreach(char key in xCharCount.Keys)
+            {
+                if (!yCharCount.ContainsKey(key))
+                {
+                    return false;
+                }
+                if(xCharCount[key] != yCharCount[key])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void Q02_CountChars(string x, Dictionary<char, int> charCount)
+        {
+            foreach (char c in x)
+            {
+                if (!charCount.ContainsKey(c))
+                {
+                    charCount[c] = 1;
+                }
+                else
+                {
+                    charCount[c]++;
+                }
+            }
+        }
+
+        public string Q03_URLify(char[] s, int length)
+        {
+            // what about trailing whitespace?
+            //   since it
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CtciCsharpTest/Q01_Tests.cs
@@ -0,0 +1,84 @@
+﻿using CtciCsharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CtciCsharpTest
+{
+    [TestClass]
+    public class Q01_IsUnique_Tests
+    {
+        Ch01 _c;
+
+        public Q01_IsUnique_Tests()
+        {
+            _c = new Ch01();
+        }
+
+        [TestMethod]
+        public void Basics()
+        {
+            Assert.IsTrue(_c.Q01_IsUnique("abc"));
+            Assert.IsFalse(_c.Q01_IsUnique("aaa"));
+        }
+
+        [TestMethod]
+        public void StrangeCases() // need confirmation from IVer
+        {
+            Assert.IsTrue(_c.Q01_IsUnique(""));
+        }
+    }
+
+    [TestClass]
+    public class Q01X_IsUnique_NoAddDSes_Tests
+    {
+        [TestMethod]
+        public void Basics()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+
+    [TestClass]
+    public class Q02_CheckPermutation_Tests
+    {
+        Ch01 _c = new Ch01();
+
+        [TestMethod]
+        public void Basics()
+        {
+            Assert.IsTrue(_c.Q02_CheckPermutation("abc", "cba"));
+            Assert.IsTrue(_c.Q02_CheckPermutation("abc", "abc"));
+            Assert.IsTrue(_c.Q02_CheckPermutation("aaa", "aaa"));
+
+            Assert.IsFalse(_c.Q02_CheckPermutation("aaa", "aba"));
+            Assert.IsFalse(_c.Q02_CheckPermutation("abc", "abd"));
+            Assert.IsFalse(_c.Q02_CheckPermutation("abc", "xyz"));
+        }
+
+        [TestMethod]
+        public void DifferentLengths()
+        {
+            Assert.IsFalse(_c.Q02_CheckPermutation("", "a"));
+            Assert.IsFalse(_c.Q02_CheckPermutation("a", "aa"));
+            Assert.IsFalse(_c.Q02_CheckPermutation("ab", "abb"));
+        }
+
+        [TestMethod]
+        public void SomewhatStrange()
+        {
+            Assert.IsTrue(_c.Q02_CheckPermutation("aa ", "a a"));
+            Assert.IsFalse(_c.Q02_CheckPermutation("aaa", "aAa"));
+        }
+    }
+
+    [TestClass]
+    public class Q03_URLify_Tests
+    {
+        Ch01 _c = new Ch01();
+
+        [TestMethod]
+        public void Sample()
+        {
+            Assert.Equals(@"Mr%20John%20Smith", _c.Q03_URLify("Mr John Smith".ToCharArray(), 13));
+        }
+    }
+}
