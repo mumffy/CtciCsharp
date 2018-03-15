8355865df77848303b5b2ee073a3406ae339230c
heibear <mumffy+alternate@gmail.com>
2018-03-14 23:24:33 -0700

    find lca without maintaining full path to node, but by returning "tuples"; more tests

--- EPI/09 Binary Trees/C09Q03.cs
+++ EPI/09 Binary Trees/C09Q03.cs
@@ -11,6 +11,54 @@ namespace EPI.C09_Binary_Trees
     {
         public static Node<T> FindLCA(BinaryTree<T> tree, Node<T> a, Node<T> b)
         {
+            //return FindLCAComplicated(tree, a, b);
+            return FindLCASimpler(tree, a, b);
+        }
+
+        private static Node<T> FindLCASimpler(BinaryTree<T> tree, Node<T> a, Node<T> b)
+        {
+            if (tree.Root == a || tree.Root == b)
+                return null;
+
+            return FindLCA(tree.Root, a, b).lca;
+        }
+
+        private static Result FindLCA(Node<T> current, Node<T> a, Node<T> b)
+        {
+            Result result = new Result(0, null);
+            if (current == null)
+                return result;
+
+            Result leftResult = FindLCA(current.Left, a, b);
+            Result rightResult = FindLCA(current.Right, a, b);
+            if (leftResult.lca != null)
+                return leftResult;
+            if (rightResult.lca != null)
+                return rightResult;
+
+            result.numFound = leftResult.numFound + rightResult.numFound;
+            if (result.numFound == 2 && result.lca == null)
+                result.lca = current;
+
+            if (current == a || current == b)
+                result.numFound++;
+
+            return result;
+        }
+
+        private class Result
+        {
+            public Result(int numFound, Node<T> lca)
+            {
+                this.numFound = numFound;
+                this.lca = lca;
+            }
+            public int numFound { get; set; }
+            public Node<T> lca { get; set; }
+        }
+
+        private static Node<T> FindLCAComplicated(BinaryTree<T> tree, Node<T> a, Node<T> b)
+        {
             if (tree.Root == a || tree.Root == b)
                 return null;
 
@@ -27,7 +75,7 @@ namespace EPI.C09_Binary_Trees
             else
                 bIndex += lengthDiff;
 
-            while(aIndex < aToRoot.Count)
+            while (aIndex < aToRoot.Count)
             {
                 if (aToRoot[aIndex] == bToRoot[bIndex])
                     return aToRoot[aIndex];
@@ -37,99 +85,143 @@ namespace EPI.C09_Binary_Trees
             return null;
         }
 
-    public static List<Node<T>> BfsFind(BinaryTree<T> tree, T target)
-    {
-        return Bfs(tree.Root, target);
-    }
-
-    public static List<Node<T>> BfsFind(BinaryTree<T> tree, Node<T> target)
-    {
-        return Bfs(tree.Root, target);
-    }
-
-    private static List<Node<T>> Bfs(Node<T> n, T target)
-    {
-        if (n == null)
-            return null;
-
-        List<Node<T>> pathToRoot;
-        if (n.Value.Equals(target))
+        public static List<Node<T>> BfsFind(BinaryTree<T> tree, T target)
         {
-            pathToRoot = new List<Node<T>>();
-            pathToRoot.Add(n);
+            return Bfs(tree.Root, target);
         }
-        else
+
+        public static List<Node<T>> BfsFind(BinaryTree<T> tree, Node<T> target)
         {
-            pathToRoot = Bfs(n.Left, target) ?? Bfs(n.Right, target) ?? null;
-            if (pathToRoot != null)
+            return Bfs(tree.Root, target);
+        }
+
+        private static List<Node<T>> Bfs(Node<T> n, T target)
+        {
+            if (n == null)
+                return null;
+
+            List<Node<T>> pathToRoot;
+            if (n.Value.Equals(target))
             {
+                pathToRoot = new List<Node<T>>();
                 pathToRoot.Add(n);
             }
-        }
-        return pathToRoot;
-    }
-
-    private static List<Node<T>> Bfs(Node<T> n, Node<T> target)
-    {
-        if (n == null)
-            return null;
-
-        List<Node<T>> pathToRoot;
-        if (n == target)
-        {
-            pathToRoot = new List<Node<T>>();
-            pathToRoot.Add(n);
-        }
-        else
-        {
-            pathToRoot = Bfs(n.Left, target) ?? Bfs(n.Right, target) ?? null;
-            if (pathToRoot != null)
+            else
             {
+                pathToRoot = Bfs(n.Left, target) ?? Bfs(n.Right, target) ?? null;
+                if (pathToRoot != null)
+                {
+                    pathToRoot.Add(n);
+                }
+            }
+            return pathToRoot;
+        }
+
+        private static List<Node<T>> Bfs(Node<T> n, Node<T> target)
+        {
+            if (n == null)
+                return null;
+
+            List<Node<T>> pathToRoot;
+            if (n == target)
+            {
+                pathToRoot = new List<Node<T>>();
                 pathToRoot.Add(n);
             }
+            else
+            {
+                pathToRoot = Bfs(n.Left, target) ?? Bfs(n.Right, target) ?? null;
+                if (pathToRoot != null)
+                {
+                    pathToRoot.Add(n);
+                }
+            }
+            return pathToRoot;
         }
-        return pathToRoot;
     }
-}
 
 
-public class C09Q03_Tests
-{
-    [Fact]
-    public void Example()
+    public class C09Q03_Tests
     {
-        BinaryTree<string> tree = getExampleTree();
-        Node<string> k = Q03<string>.BfsFind(tree, "K")?[0];
-        Node<string> m = Q03<string>.BfsFind(tree, "M")?[0];
-        Node<string> n = Q03<string>.BfsFind(tree, "N")?[0];
-        Assert.NotNull(k);
-        Assert.NotNull(m);
-        Assert.NotNull(n);
-        Assert.Equal(m, k.Left.Right);
-        Assert.Equal(n, k.Right);
+        private BinaryTree<string> getExampleTree()
+        {
+            BinaryTree<string> tree = new BinaryTree<string>();
+            tree.Root = new Node<string>("A",
+                left: new Node<string>("B",
+                    left: new Node<string>("D"),
+                    right: new Node<string>("E")),
+                right: new Node<string>("C",
+                    left: new Node<string>("K",
+                        left: new Node<string>("L",
+                            right: new Node<string>("M")),
+                        right: new Node<string>("N")),
+                    right: new Node<string>("P"))
+                );
+            return tree;
+        }
 
-        Node<string> lcaNode = Q03<string>.FindLCA(tree, m, n);
-        Assert.Equal(k, lcaNode);
-        Assert.Equal("K", lcaNode.Value);
-    }
+        [Fact]
+        public void Example()
+        {
+            BinaryTree<string> tree = getExampleTree();
+            Node<string> k = Q03<string>.BfsFind(tree, "K")?[0];
+            Node<string> m = Q03<string>.BfsFind(tree, "M")?[0];
+            Node<string> n = Q03<string>.BfsFind(tree, "N")?[0];
+            Assert.NotNull(k);
+            Assert.NotNull(m);
+            Assert.NotNull(n);
+            Assert.Equal(m, k.Left.Right);
+            Assert.Equal(n, k.Right);
 
-    private BinaryTree<string> getExampleTree()
-    {
-        BinaryTree<string> tree = new BinaryTree<string>();
-        tree.Root = new Node<string>("A",
-            left: new Node<string>("B",
-                left: new Node<string>("D"),
-                right: new Node<string>("E")),
-            right: new Node<string>("C",
-                left: new Node<string>("K",
-                    left: new Node<string>("L",
-                        right: new Node<string>("M")),
-                    right: new Node<string>("N")),
-                right: new Node<string>("P"))
-            );
-        return tree;
+            Node<string> lcaNode = Q03<string>.FindLCA(tree, m, n);
+            Assert.Equal(k, lcaNode);
+            Assert.Equal("K", lcaNode.Value);
+        }
+
+        [Fact]
+        public void NoLCA()
+        {
+            BinaryTree<string> tree = getExampleTree();
+            Node<string> k = Q03<string>.BfsFind(tree, "K")?[0];
+            Assert.NotNull(k);
+
+            Node<string> lcaNode = Q03<string>.FindLCA(tree, k, tree.Root);
+            Assert.Equal(null, lcaNode);
+        }
+
+        [Fact]
+        public void Test01()
+        {
+            BinaryTree<string> tree = getExampleTree();
+            Node<string> d = Q03<string>.BfsFind(tree, "D")?[0];
+            Node<string> n = Q03<string>.BfsFind(tree, "N")?[0];
+            Assert.NotNull(d);
+            Assert.NotNull(n);
+
+            Node<string> lcaNode = Q03<string>.FindLCA(tree, d, n);
+            Assert.Equal(tree.Root, lcaNode);
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            BinaryTree<string> tree = getExampleTree();
+            Node<string> c = Q03<string>.BfsFind(tree, "C")?[0];
+            Node<string> l = Q03<string>.BfsFind(tree, "L")?[0];
+            Node<string> p = Q03<string>.BfsFind(tree, "P")?[0];
+            Assert.NotNull(c);
+            Assert.NotNull(l);
+            Assert.NotNull(p);
+            Assert.Equal(l, c.Left.Left);
+            Assert.Equal(p, c.Right);
+
+            Node<string> lcaNode = Q03<string>.FindLCA(tree, l, p);
+            Assert.Equal(c, lcaNode);
+            Assert.Equal("C", lcaNode.Value);
+        }
+
+
     }
-}
 
 
 
