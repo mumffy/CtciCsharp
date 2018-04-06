d0e6c238abe1fecc37c73a1eadff2b73e1fc43f6
heibear <mumffy+alternate@gmail.com>
2018-04-05 18:19:26 -0700

    more renaming

---
+++ EPI/11 Searching/C11Q02.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EPI.C11_Search
+{
+    static class Q02
+    {
+        
+    }
+
+    public class C11Q02_Tests
+    {
+        [Fact]
+        public void Example()
+        {
+            
+        }
+    }
+}
