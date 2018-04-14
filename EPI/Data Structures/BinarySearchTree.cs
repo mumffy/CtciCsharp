55eecbd1258638993da01cf40f9cf2d9862c31bd
heibear <mumffy+alternate@gmail.com>
2018-04-13 23:27:59 -0700

    BST Find method

--- EPI/Data Structures/BinarySearchTree.cs
+++ EPI/Data Structures/BinarySearchTree.cs
@@ -10,5 +10,22 @@ namespace EPI.DataStructures.BinarySearchTree
     public class BinarySearchTree<T> : BinaryTree<T> where T : IComparable
     {
         public int Count { get; set; }
+
+        public Node<T> Find(T value)
+        {
+            return Find(Root, value);
+        }
+
+        private Node<T> Find(Node<T> node, T value)
+        {
+            if (node == null)
+                return null;
+
+            if (node.Value.CompareTo(value) < 0)
+                return Find(node.Right, value);
+            if (node.Value.CompareTo(value) > 0)
+                return Find(node.Left, value);
+            return node;
+        }
     }
 }
