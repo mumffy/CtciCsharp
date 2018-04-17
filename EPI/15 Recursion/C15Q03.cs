d9fa4750c930c02b13e8cb16976f9a85bea21931
heibear <mumffy+alternate@gmail.com>
2018-04-16 18:24:54 -0700

    N Queens WIP... have to come back to this later...

---
+++ EPI/15 Recursion/C15Q03.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace EPI.C15_Recursion
+{
+    public class Q03
+    {
+       
+    }
+
+    public class C15Q03_Tests
+    {
+        private readonly ITestOutputHelper output;
+
+        public C15Q03_Tests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+    }
+}
