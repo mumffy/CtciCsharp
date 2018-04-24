8ca7140d2acd0b5b1e69180e2481326a1b79b61c
heibear <mumffy+alternate@gmail.com>
2018-04-23 19:09:46 -0700

    cop-out "ThreeSum" solution with hash table

---
+++ EPI/17 Greedy Algorithms and Invariants/C17Q04.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPI.C17_Greedy_Algorithms_and_Invariants
+{
+    class Q04
+    {
+        public static bool HasThreeSum(int[] array, int target)
+        {
+            HashSet<int> numbers = new HashSet<int>(array);
+            int remaining;
+
+            foreach (int x in array)
+            {
+                remaining = target - x;
+                foreach(int y in array)
+                    if (numbers.Contains(remaining - y))
+                        return true;
+            }
+
+            return false;
+        }
+    }
+
+    [TestClass]
+    public class C17Q04_Tests
+    {
+        int[] exampleArray;
+        public C17Q04_Tests()
+        {
+            exampleArray = new int[] { 11, 2, 5, 7, 3 };
+        }
+
+        [TestMethod]
+        public void ExampleHasSum()
+        {
+            Assert.IsTrue(Q04.HasThreeSum(exampleArray, 21));
+        }
+
+        [TestMethod]
+        public void ExampleNoSum()
+        {
+            Assert.IsFalse(Q04.HasThreeSum(exampleArray, 22));
+        }
+    }
+}
